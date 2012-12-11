87f64defd84ba85654e583ce794c0ddf93218363
Leon <leonyapkl@gmail.com>
2012-12-11 13:00:46 +0800

    basic down page and route

--- ci/application/views/templates/header.php
+++ ci/application/views/templates/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $title ?></title>
+<link rel="stylesheet" type="text/css" href="css/core.css">
+<title><?php echo $title ?></title>
 </head>
 <body>
-	<h1>The blank ______</h1>
+<h1>The blank ______</h1>
