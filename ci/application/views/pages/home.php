b677af6c00349841131f416f1b23cbd074b820c1
Leon <leonyapkl@gmail.com>
2012-12-13 11:35:48 +0800

    12 box setup, clean up css

--- ci/application/views/pages/home.php
+++ ci/application/views/pages/home.php
@@ -5,11 +5,18 @@ $defaultHome = array(
 	'box2' => 'box 2',
 	'box3' => 'box 3',
 	'box4' => 'box 4',
-	'box5' => 'box 5'
+	'box5' => 'box 5',
+	'box6' => 'box 6',
+	'box7' => 'box 7',
+	'box8' => 'box 8',
+	'box9' => 'box 9',
+	'box10' => 'box 10',
+	'box11' => 'box 11',
+	'box12' => 'box 12'
 )
 ?>
 <div class = "wrapper">
-<h1><?php echo $defaultHome['topic']?> is _______</h1>
+<div class = "topic"><h1><?php echo $defaultHome['topic']?> is _______</h1></div>
 
 <div class = "box_frame" id = "pos1" style="background-color:rgb(15,99,30);"><div class ="boxContent" id = "boxContent1">
 <b><?php echo $defaultHome['box1']?></b>
@@ -30,4 +37,33 @@ $defaultHome = array(
 <div class = "box_frame" id = "pos5"><div class ="boxContent">
 <b><?php echo $defaultHome['box5']?></b>
 </div></div>
+
+<div class = "box_frame" id = "pos6"><div class ="boxContent">
+<b><?php echo $defaultHome['box6']?></b>
+</div></div>
+
+<div class = "box_frame" id = "pos7"><div class ="boxContent">
+<b><?php echo $defaultHome['box7']?></b>
+</div></div>
+
+<div class = "box_frame" id = "pos8"><div class ="boxContent">
+<b><?php echo $defaultHome['box8']?></b>
+</div></div>
+
+<div class = "box_frame" id = "pos9"><div class ="boxContent">
+<b><?php echo $defaultHome['box9']?></b>
+</div></div>
+
+<div class = "box_frame" id = "pos10"><div class ="boxContent">
+<b><?php echo $defaultHome['box10']?></b>
+</div></div>
+
+<div class = "box_frame" id = "pos11"><div class ="boxContent">
+<b><?php echo $defaultHome['box11']?></b>
+</div></div>
+
+<div class = "box_frame" id = "pos12"><div class ="boxContent">
+<b><?php echo $defaultHome['box12']?></b>
+</div></div>
+
 </div>
