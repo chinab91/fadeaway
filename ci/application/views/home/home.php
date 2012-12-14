58d17421dad344326755810941041f88c7db519a
Leon <leonyapkl@gmail.com>
2012-12-14 22:22:05 +0800

    rewrite home page, change css, change box numbering.

--- ci/application/views/home/home.php
+++ ci/application/views/home/home.php
@@ -1,60 +1,31 @@
 <div class = "wrapper">
 <div class = "topic"><h1><?php echo $topic['topic']?> _______</h1></div>
 
-<div class = "box_frame" id = "pos1" style="background-color:rgb(
-    <?php echo $entries['0']['bground_R']?>,
-    <?php echo $entries['0']['bground_G']?>,
-    <?php echo $entries['0']['bground_B']?>
-     ); color:rgb(
-    <?php echo $entries['0']['font_R']?>,
-    <?php echo $entries['0']['font_G']?>,
-    <?php echo $entries['0']['font_B']?>
-     );"><div class ="boxContent" id = "boxContent1">
-<b><?php echo $entries['0']['content']?></b>
+<div class = "box_frame" id = "pos0"><div class ="box_content" id = "boxContent1">
+<b>Fill this</b>
 </div></div>
 
-<div class = "box_frame" id = "pos2"><div class ="boxContent" id = "boxContent1">
-<b><?php echo $entries['1']['content']?></b>
-</div></div>
+<?php 
+$counter = 1;
+foreach ($entries as $entry){
+echo "<div class = 'box_frame' id = 'pos$counter'"; 
+$style = "style=\"background-color:rgb(".
+        $entry['bground_R'].",".$entry['bground_G'].",".$entry['bground_B']
+        ."); ";
+$char_colour = "colour:rgb(".
+        $entry['font_R'].",".$entry['font_G'].",".$entry['font_B']
+        .");\">";//end style //end start div box_frame
+echo $style.$char_colour;
+echo "<div class =\"box_content\">\n";//div box_content
+echo "<b>".$entry['content']."</b>\n";
+echo "</div></div>\n";
+$counter++;
+}      
+for($unfilled_box = $counter;$unfilled_box <= 11; $unfilled_box++){
+echo "<div class = 'box_frame' id = 'pos$unfilled_box'>\n"; 
+echo "<div class =\"box_content\">\n";//div box_content
+echo "</div></div>\n";
+}
 
-<div class = "box_frame" id = "pos3"><div class ="boxContent">
-<b></b>
-</div></div>
-
-<div class = "box_frame" id = "pos4"><div class ="boxContent">
-<b></b>
-</div></div>
-
-<div class = "box_frame" id = "pos5"><div class ="boxContent">
-<b></b>
-</div></div>
-
-<div class = "box_frame" id = "pos6"><div class ="boxContent">
-<b></b>
-</div></div>
-
-<div class = "box_frame" id = "pos7"><div class ="boxContent">
-<b></b>
-</div></div>
-
-<div class = "box_frame" id = "pos8"><div class ="boxContent">
-<b></b>
-</div></div>
-
-<div class = "box_frame" id = "pos9"><div class ="boxContent">
-<b></b>
-</div></div>
-
-<div class = "box_frame" id = "pos10"><div class ="boxContent">
-<b></b>
-</div></div>
-
-<div class = "box_frame" id = "pos11"><div class ="boxContent">
-<b></b>
-</div></div>
-
-<div class = "box_frame" id = "pos12"><div class ="boxContent">
-<b></b>
-</div></div>
-
+?>
 </div>
