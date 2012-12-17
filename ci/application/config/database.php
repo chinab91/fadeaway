4806e193179f2f8d9d6193205810c63b9942cc42
Leon <leonyapkl@gmail.com>
2012-12-18 00:12:56 +0800

    fix database

--- ci/application/config/database.php
+++ ci/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'awdko00';
-$db['default']['database'] = 'web_db';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
